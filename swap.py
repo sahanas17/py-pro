1da7ac6aa11518b6e94f22389e1c5af61cde26f3
Sahana Rao <sahanared17@gmail.com>
2024-12-06 19:46:33 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,9 @@
+def swap(a, b):
+    print("values of a and b before swapping:", a, b)
+    temp = a
+    a = b
+    b = temp
+    print("values of a and b after swapping:", a, b)
+
+print( "Program to swap two numbers" )  
+swap(2, 5)
